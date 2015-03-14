2ea034a816ba942e91a15f8a702ed5ad05926f4f
abakarEgrey <egreabakar@yahoo.fr>
2015-03-14 11:13:44 +0100

    #3 commit reponse exigence

---
+++ src/main/java/dcll/amah/mysimplestack/Stack.java
@@ -0,0 +1,44 @@
+package dcll.amah.mysimplestack;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by mahamat on 28/02/15.
+ * Un commentaire repondant a l'exigence #3
+ */
+public class Stack implements SimpleStack {
+
+    private int size;
+    //private int
+    private boolean isEmpty;
+
+    public Stack()
+    {
+        this.isEmpty = true;
+        this.size = 0;
+    }
+    @Override
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void push(Item item) {
+
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return null;
+    }
+}
